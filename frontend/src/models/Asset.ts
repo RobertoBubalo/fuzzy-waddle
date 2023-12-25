085b92c171e1294520b9a8326d583abab3f56aa0
RobertoBubalo <robertobubalo@gmail.com>
2023-12-25 19:53:01 +0000

    Feat: dummy selector and values

---
+++ frontend/src/models/Asset.ts
@@ -0,0 +1,7 @@
+export interface Asset {
+  id: number
+  name: string
+  tickerSymbol: string
+  shares: number
+  shareValue: number
+}
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import FuzzyWaddleView from '../views/FuzzyWaddleView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,15 +7,23 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: FuzzyWaddleView
     },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import('../views/AboutView.vue')
+    // },
     {
-      path: '/about',
-      name: 'about',
+      path: '/fuzzy-waddle',
+      name: 'fuzzy-waddle',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/FuzzyWaddleView.vue')
     }
   ]
 })
