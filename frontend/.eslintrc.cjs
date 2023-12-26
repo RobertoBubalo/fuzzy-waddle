afaa6087a613dbbb63ddb8a61a50430d2368e0ea
RobertoBubalo <robertobubalo@gmail.com>
2023-12-26 19:38:39 +0000

    Config, lint and formatting

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,25 +1,41 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require("@rushstack/eslint-patch/modern-module-resolution")
 
 module.exports = {
-  root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
-  ],
-  overrides: [
-    {
-      files: [
-        'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}'
-      ],
-      'extends': [
-        'plugin:cypress/recommended'
-      ]
-    }
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  }
+    root: true,
+    extends: [
+        "plugin:vue/vue3-essential",
+        "eslint:recommended",
+        "@vue/eslint-config-typescript",
+        "@vue/eslint-config-prettier/skip-formatting",
+    ],
+    overrides: [
+        {
+            files: ["cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}"],
+            extends: ["plugin:cypress/recommended"],
+        },
+    ],
+    parserOptions: {
+        ecmaVersion: "latest",
+    },
+    rules: {
+        "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "prettier/prettier": [
+            "warn",
+            {
+                tabWidth: 4,
+                printWidth: 120,
+            },
+        ],
+        "eol-last": "off",
+        "max-lines": [
+            "warn",
+            {
+                max: 500,
+                skipComments: true,
+                skipBlankLines: true,
+            },
+        ],
+    },
 }
